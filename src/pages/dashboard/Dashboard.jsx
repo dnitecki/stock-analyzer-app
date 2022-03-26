b782fb9bf75c5f68d8ec2371b8cf06aa5317e3ef
Dominck Nitecki <dominick.nitecki@outlook.com>
2022-03-25 20:02:49 -0500

    dashboard layout and mock data

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 import "./Dashboard.scss";
+import Card from "../../components/card/Card";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="body">
+      <div className="dashboard">
+        <div className="header">
+          <Card>Header</Card>
+        </div>
+        <div className="chart">
+          <Card>Chart</Card>
+        </div>
+        <div className="overview">
+          <Card>Overview</Card>
+        </div>
+        <div className="details">
+          <Card>Details</Card>
+        </div>
+      </div>
+    </div>
+  );
 }
