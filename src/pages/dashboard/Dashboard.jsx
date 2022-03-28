924e3e8f694dbb8c9b4e7ec451279006d6a0b0a6
Dominck Nitecki <dominick.nitecki@outlook.com>
2022-03-28 13:08:48 -0500

    wire overview and details to data

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Dashboard.scss";
-
 import Header from "../../components/header/Header";
-import { mockCompanyDetails } from "../../constants/mock";
+// import { mockCompanyDetails } from "../../constants/mock";
 import Details from "../../components/details/Details";
 import Overview from "../../components/overview/Overview";
 import Chart from "../../components/chart/Chart";
+import StockContext from "../../components/context/StockContext";
+import { fetchQuote, fetchStockDetails } from "../../api/stock-api";
 
 export default function Dashboard() {
+  const { stockSymbol } = useContext(StockContext);
+  const [stockDetails, setStockDetails] = useState({});
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    const updateStockDetails = async () => {
+      try {
+        const result = await fetchStockDetails(stockSymbol);
+        setStockDetails(result);
+      } catch (error) {
+        setStockDetails({});
+        console.log(error);
+      }
+    };
+    const updateStockOverview = async () => {
+      try {
+        const result = await fetchQuote(stockSymbol);
+        setQuote(result);
+      } catch (error) {
+        setQuote({});
+        console.log(error);
+      }
+    };
+
+    updateStockDetails();
+    updateStockOverview();
+  }, [stockSymbol]);
   return (
     <div>
       <div className="dashboard">
         <div className="dashboard-header">
-          <Header
-            name={mockCompanyDetails.name}
-            ticker={mockCompanyDetails.ticker}
-          />
+          <Header name={stockDetails.name} ticker={stockSymbol} />
         </div>
         <div className="dashboard-chart">
           <Chart />
         </div>
         <div className="dashboard-overview">
           <Overview
-            symbol={mockCompanyDetails.ticker}
-            price={300}
-            change={30}
-            changePercent={10.0}
-            currency={"USD"}
+            symbol={stockSymbol}
+            price={quote.pc}
+            change={quote.d}
+            changePercent={quote.dp}
+            currency={stockDetails.currency}
           />
         </div>
         <div className="dashboard-details">
-          <Details details={mockCompanyDetails} />
+          <Details details={stockDetails} />
         </div>
       </div>
     </div>
