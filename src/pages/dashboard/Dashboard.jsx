f9e4e0da62558a22432064c755290aca92b6c2f9
Dominck Nitecki <dominick.nitecki@outlook.com>
2022-03-31 08:48:16 -0500

    fix fech bug

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -17,7 +17,7 @@ export default function Dashboard() {
   const { stockSymbol } = useContext(StockContext);
   const [stockDetails, setStockDetails] = useState({});
   const [quote, setQuote] = useState({});
-  const [news, setNews] = useState({});
+  const [news, setNews] = useState([]);
 
   useEffect(() => {
     const updateStockDetails = async () => {
