f5d311fd817d8e600b814c34063a391cba2b93fd
Dominck Nitecki <dominick.nitecki@outlook.com>
2022-04-01 14:53:09 -0500

    add progress spinner

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -7,6 +7,7 @@ import { searchSymbols } from "../../api/stock-api";
 export default function Search() {
   const [input, setInput] = useState("");
   const [bestMatches, setBestMatches] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const clear = () => {
     setInput("");
@@ -14,47 +15,55 @@ export default function Search() {
   };
 
   const updateBestMatches = async () => {
+    setIsLoading(true);
     try {
       if (input) {
         const searchResults = await searchSymbols(input);
         const result = searchResults.result;
         setBestMatches(result);
+        setIsLoading(false);
       }
     } catch (error) {
       setBestMatches([]);
       console.log(error);
+      setIsLoading(false);
     }
   };
   return (
     <div>
-      <span className="searchbar">
-        <input
-          type="text"
-          className="input"
-          value={input}
-          placeholder="Search stock..."
-          onChange={(event) => {
-            setInput(event.target.value);
-          }}
-          onKeyPress={(event) => {
-            if (event.key === "Enter") {
-              updateBestMatches();
-            }
-          }}
-        />
-        <div className="icon">
-          {input && (
-            <button>
-              <i className="pi pi-times clear-icon" onClick={clear} />
+      <div className="searchbar-container">
+        <span className="searchbar">
+          <input
+            type="text"
+            className="input"
+            value={input}
+            placeholder="Search stock..."
+            onChange={(event) => {
+              setInput(event.target.value);
+            }}
+            onKeyPress={(event) => {
+              if (event.key === "Enter") {
+                updateBestMatches();
+              }
+            }}
+          />
+          <div className="icon">
+            {input && (
+              <button>
+                <i className="pi pi-times clear-icon" onClick={clear} />
+              </button>
+            )}
+          </div>
+          <div className="search">
+            <button onClick={updateBestMatches}>
+              <i className="pi pi-search search-icon" />
             </button>
-          )}
+          </div>
+        </span>
+        <div className="spinner">
+          {isLoading ? <div className="progress-spinner"></div> : null}
         </div>
-        <div className="search">
-          <button onClick={updateBestMatches}>
-            <i className="pi pi-search search-icon" />
-          </button>
-        </div>
-      </span>
+      </div>
       {input && bestMatches.length > 0 ? (
         <SearchResults results={bestMatches} />
       ) : null}
