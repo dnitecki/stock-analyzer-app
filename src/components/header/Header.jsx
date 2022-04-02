ef023aac3f5ba013aa85330a10d5dbab7d37a570
Dominck Nitecki <dominick.nitecki@outlook.com>
2022-04-01 21:58:41 -0500

    header updates

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,12 +5,12 @@ import Search from "../search/Search";
 export default function Header({ logo, name, ticker }) {
   return (
     <div className="header-container">
-      <h1 className="header-text">
+      <div className="header-text">
         <div className="logo-container">
           <img className="header-logo" src={logo} alt={name} />
         </div>
         {name}&nbsp;({ticker})
-      </h1>
+      </div>
       <Search />
     </div>
   );
